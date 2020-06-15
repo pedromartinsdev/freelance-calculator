0006c74c39f4d8ed518817654e662bbd04ac3424
Pedro Martins <pedrohenriquemartins@live.com>
2020-06-15 19:55:20 -0300

    script.js modification

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const projectValueInput = parseFloat(document.querySelector("input[name=project-value]").value)
-const workedHoursInput = parseFloat(document.querySelector("input[name=worked-hours]").value)
-const workedDaysInput = parseFloat(document.querySelector("input[name=worked-days]").value)
-const vacationDaysInput = parseFloat(document.querySelector("input[name=vacation-days]").value)
-const valueHourElement = document.querySelector("#result")
-
-
 function calculo() {
+  var projectValueInput = document.querySelector('input[name="project-value"]').value.toString()
+  var workedHoursInput = document.querySelector('input[name="worked-hours"]').value
+  var workedDaysInput = document.querySelector('input[name="worked-days"]').value
+  var vacationDaysInput = document.querySelector('input[name="vacation-days"]').value
+  var valueHourElement = document.querySelector("#result")
+
   let hourValue
 
   hourValue = 12 * projectValueInput / (52.1 * (workedHoursInput * workedDaysInput) - ((workedHoursInput * workedDaysInput) * vacationDaysInput))
@@ -14,13 +13,9 @@ function calculo() {
   Math.round(hourValue)
   hourValue = parseFloat(hourValue).toFixed(2)
 
-  console.log(hourValue)
-  if (hourValue == "NaN") {
+  if (isNaN(hourValue)) {
     valueHourElement.innerHTML = "R$ 0,00"
   } else {
     valueHourElement.innerHTML = "R$ " + Math.round(hourValue) + ",00"
   }
 }
-
-
-
